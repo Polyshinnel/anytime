e6b5943ea89354f26103df5f8ffb82047fc1969a
Polyshinnel <polyshinnel@gmail.com>
2024-10-06 22:05:27 +0300

    add some blocks to main page

---
+++ src/assets/js/main.js
@@ -0,0 +1,96 @@
+document.addEventListener("DOMContentLoaded", () => {
+    if(document.querySelector('.more-text')) {
+        addReadmoreHeight();
+        let moreTextBtns = document.querySelectorAll('.more-text')
+        moreTextBtns.forEach((button) => {
+            button.addEventListener('click', () => {
+                readMore(button)
+            })
+        })
+    }
+    
+    if(document.querySelector('.product-list__item-tab-head__item')) {
+        let tabBtns = document.querySelectorAll('.product-list__item-tab-head__item')
+        tabBtns.forEach((item) => {
+            item.addEventListener('click', () => {
+                changeTab(item)
+            })
+        })
+    }
+
+    if(document.querySelector('.product-quantity-btn')) {
+        document.querySelectorAll('.product-quantity-btn-minus').forEach((btn) => {
+            btn.addEventListener('click', () => {
+                let input = btn.parentElement.querySelector('.product-quantity-input')
+                let currVal = parseInt(input.value) - 1
+                if(currVal < 1) {
+                    currVal = 1
+                }
+                input.value = currVal
+            })
+        })
+
+        document.querySelectorAll('.product-quantity-btn-plus').forEach((btn) => {
+            btn.addEventListener('click', () => {
+                let input = btn.parentElement.querySelector('.product-quantity-input')
+                let currVal = parseInt(input.value) + 1
+                input.value = currVal
+            })
+        })
+    }
+})
+
+const addReadmoreHeight = () => {
+    let textBlock = document.querySelectorAll('.product-list__item-description-block__text');
+    textBlock.forEach((item) => {
+        console.log(calculateTotalHeight(item))
+        let firstSection = item.querySelector('p:first-child')
+        item.style.maxHeight = (parseInt(firstSection.clientHeight) + 10)+'px'
+    })
+}
+
+const calculateMinHeigth = (textBlock) => {
+    let firstSection = textBlock.querySelector('p:first-child')
+    let minHeight = (parseInt(firstSection.clientHeight) + 10)+'px'
+    return minHeight
+}
+
+const calculateTotalHeight = (textBlock) => {
+    let totalHeight = 0
+    let sectionItems = textBlock.querySelectorAll('p')
+    sectionItems.forEach((unit) => {
+        totalHeight += (parseInt(unit.clientHeight) + 10);
+    })
+    return totalHeight + 'px';
+}
+
+const readMore = (button) => {
+    console.log(button)
+    let container = button.parentElement.querySelector('.product-list__item-description-block__text')
+    if(container.classList.contains('active')) {
+        container.classList.remove('active')
+        container.style.maxHeight = calculateMinHeigth(container)
+        button.textContent = 'Подробнее...'
+    } else {
+        container.classList.add('active');
+        container.style.maxHeight = calculateTotalHeight(container)
+        button.textContent = 'Скрыть'
+    }
+}
+
+const changeTab = (tabBtn) => {
+    let attrName = tabBtn.getAttribute('data-item')
+    let tabs = document.querySelectorAll('.product-list__item-tab')
+    let tabBtns = document.querySelectorAll('.product-list__item-tab-head__item')
+    tabBtns.forEach((btn) => {
+        btn.classList.remove('product-list__item-tab-head__item_active')
+    })
+    tabBtn.classList.add('product-list__item-tab-head__item_active')
+    tabs.forEach((tab) => {
+        tab.classList.remove('product-list__item-tab_active')
+        if(tab.getAttribute('data-item') == attrName) {
+            tab.classList.add('product-list__item-tab_active')
+        }
+    })
+}
+
